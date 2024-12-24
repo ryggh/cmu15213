1ac764e3d0dbf28435be593a0f204b04f063b357
dyh <1625132542@qq.com>
2024-12-24 09:21:02 +0800

    a few modification

--- malloclab-handout/mm.c
+++ malloclab-handout/mm.c
@@ -5,6 +5,7 @@
  * efficient and space-less
  */
 #include <assert.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -46,23 +47,24 @@ team_t team = {
 #define GET_SIZE(p) (GET(p) & ~0x7)
 #define GET_ALLOC(p) (GET(p) & 0x1)
 #define HDRP(bp) ((char *)(bp) - WSIZE)
-#define FTRP(bp) ((char *)(bp) + GET_SIZE(bp) - DSIZE)
+#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
 #define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)))
-#define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE((char *)(bp) - DSIZE))
+#define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
 #define GET_POINT(p) (*(char **)(p))
 #define PUT_POINT(p, val) (*(char **)(p) = (val))
 #define PREP(bp) (bp)
-#define SUCP(bp) ((char *)(bp) + DSIZE)
+#define SUCP(bp) ((char *)(bp) + WSIZE)
 
 // global variable
 char *heap_listp;
+char **list_32;
 char **list_64;
 char **list_128;
 char **list_256;
 char **list_512;
 char **list_1024;
 char **list_2048;
-char **list_4096;
+char **list_infinite;
 
 // helper function
 static void insert_list(void *bp, void **list);
@@ -71,33 +73,42 @@ static void *coalesce(void *bp);
 static void *extend_heap(size_t size);
 static char *head_pointer(char *bp);
 static void isinCorrectfreelist();
-static void heap_cheker();
+static void heap_checker();
 static void pop_block(char *bp);
 static void *find_block(size_t size);
 static void place(char *bp, size_t size);
+#define CHECKHEAP(lineno)                                                      \
+  printf("%d  in function %s\n", __LINE__, __func__);                          \
+  heap_checker();
 /*
  * mm_init - initialize the malloc package.
  */
 // TODO:initialize
 int mm_init(void) {
-  if ((heap_listp = mem_sbrk(9 * DSIZE)) == (void *)-1) {
+  if ((heap_listp = mem_sbrk(12 * WSIZE)) == (void *)-1) {
     return -1;
   }
   PUT(heap_listp, 0);
   PUT_POINT(heap_listp + WSIZE, NULL);
-  list_64 = heap_listp + WSIZE;
-  PUT_POINT(list_64 + DSIZE, NULL);
-  PUT_POINT(list_64 + 2 * DSIZE, NULL);
-  PUT_POINT(list_64 + 3 * DSIZE, NULL);
-  PUT_POINT(list_64 + 4 * DSIZE, NULL);
-  PUT_POINT(list_64 + 5 * DSIZE, NULL);
-  PUT_POINT(list_64 + 6 * DSIZE, NULL);
-  PUT_POINT(list_64 + 7 * DSIZE, NULL);
-  list_4096 = list_64 + 7 * DSIZE;
-  PUT(list_4096 + WSIZE, PACK(8, 1));
-  PUT(list_4096 + 2 * WSIZE, PACK(8, 1));
-  PUT(list_4096 + 3 * WSIZE, PACK(0, 1));
-  heap_listp = list_4096 + 2 * WSIZE;
+  list_32 = (char **)(heap_listp + WSIZE);
+  PUT_POINT(list_32 + 1, NULL);
+  list_64 = list_32 + 1;
+  PUT_POINT(list_32 + 2, NULL);
+  list_128 = list_64 + 1;
+  PUT_POINT(list_32 + 3, NULL);
+  list_256 = list_128 + 1;
+  PUT_POINT(list_32 + 4, NULL);
+  list_512 = list_256 + 1;
+  PUT_POINT(list_32 + 5, NULL);
+  list_1024 = list_512 + 1;
+  PUT_POINT(list_32 + 6, NULL);
+  list_2048 = list_1024 + 1;
+  PUT_POINT(list_32 + 7, NULL);
+  list_infinite = list_2048 + 1;
+  PUT(list_infinite + 1, PACK(8, 1));
+  PUT(list_infinite + 2, PACK(8, 1));
+  PUT(list_infinite + 3, PACK(0, 1));
+  heap_listp = (char *)(list_infinite + 2);
   if (extend_heap(CHUNKSIZE) == NULL) {
     return -1;
   }
@@ -112,8 +123,8 @@ void *mm_malloc(size_t size) {
   size_t asize;
   char *bp;
   size_t extendsize;
-  asize = ALIGN(size + 3 * DSIZE);
-  if (find_block(asize) != NULL) {
+  asize = ALIGN(size + DSIZE);
+  if ((bp = find_block(asize)) != NULL) {
     pop_block(bp);
     place(bp, asize);
     return bp;
@@ -124,6 +135,8 @@ void *mm_malloc(size_t size) {
   }
   pop_block(bp);
   place(bp, asize);
+  printf("malloc a block which size is %zu\n", size);
+  CHECKHEAP(lineno)
   return bp;
 }
 
@@ -133,7 +146,10 @@ void *mm_malloc(size_t size) {
 void mm_free(void *ptr) {
   PUT(HDRP(ptr), PACK(GET_SIZE(HDRP(ptr)), 0));
   PUT(FTRP(ptr), PACK(GET_SIZE(HDRP(ptr)), 0));
+  coalesce(ptr);
   insert_list(ptr, choose_list(GET_SIZE(HDRP(ptr))));
+  printf("free a block\n");
+  CHECKHEAP(lineno)
 }
 
 /*
@@ -160,18 +176,47 @@ static void insert_list(void *bp, void **list) {
   char *head = *list;
   *list = (char *)bp;
   PUT_POINT(SUCP(bp), head);
+  PUT_POINT(PREP(bp), (char *)list);
+  if (head) {
+    PUT_POINT(PREP(GET_POINT(SUCP(bp))), bp);
+  }
+  PUT(HDRP(bp), PACK(GET_SIZE(HDRP(bp)), 0));
+  PUT(FTRP(bp), PACK(GET_SIZE(HDRP(bp)), 0));
+  CHECKHEAP(lineno)
 }
 
 static void pop_block(char *bp) {
   // if the node is the first node
-  if (GET_POINT(PREP(bp)) < heap_listp) {
-    GET_POINT(PREP(bp)) = GET_POINT(SUCP(bp));
+  int isheadnode = (GET_POINT(PREP(bp)) < heap_listp);
+  int nextisnull = (GET_POINT(SUCP(bp)) == NULL);
+  if (isheadnode && !nextisnull) {
+    PUT_POINT(PREP(bp), GET_POINT(SUCP(bp)));
+    PUT_POINT(PREP(GET_POINT(SUCP(bp))), GET_POINT(PREP(bp)));
+    PUT(HDRP(bp), PACK(GET_SIZE(HDRP(bp)), 1));
+    PUT(FTRP(bp), PACK(GET_SIZE(HDRP(bp)), 1));
   }
-  GET_POINT(SUCP(PREV_BLKP(bp))) = GET_POINT(SUCP(bp));
-  GET_POINT(PREP(NEXT_BLKP(bp))) = GET_POINT(PREP(bp));
+  if (!isheadnode && nextisnull) {
+    PUT_POINT(SUCP(GET_POINT(PREP(bp))), NULL);
+    PUT(HDRP(bp), PACK(GET_SIZE(HDRP(bp)), 1));
+    PUT(FTRP(bp), PACK(GET_SIZE(HDRP(bp)), 1));
+  }
+  if (isheadnode && nextisnull) {
+    PUT_POINT(GET_POINT(PREP(bp)), NULL);
+    PUT(HDRP(bp), PACK(GET_SIZE(HDRP(bp)), 1));
+    PUT(FTRP(bp), PACK(GET_SIZE(HDRP(bp)), 1));
+  } else {
+    GET_POINT(SUCP(GET_POINT(PREP(bp)))) = GET_POINT(SUCP(bp));
+    GET_POINT(PREP(GET_POINT(SUCP(bp)))) = GET_POINT(PREP(bp));
+    PUT(HDRP(bp), PACK(GET_SIZE(HDRP(bp)), 1));
+    PUT(FTRP(bp), PACK(GET_SIZE(HDRP(bp)), 1));
+  }
+  CHECKHEAP(lineno);
 }
 
 static void *choose_list(size_t size) {
+  if (size >= 16 && size < 32) {
+    return list_32;
+  }
   if (size >= 32 && size < 64) {
     return list_64;
   }
@@ -190,8 +235,8 @@ static void *choose_list(size_t size) {
   if (size >= 1024 && size < 2048) {
     return list_2048;
   }
-  if (size >= 2048 && size <= 4096) {
-    return list_4096;
+  if (size >= 2048) {
+    return list_infinite;
   }
   return NULL;
 }
@@ -225,20 +270,24 @@ static void *coalesce(void *bp) {
     bp = PREV_BLKP(bp);
     insert_list(bp, choose_list(size));
   }
+  CHECKHEAP(lineno)
   return bp;
 }
 
 static void *extend_heap(size_t size) {
   char *bp;
-  if ((long)(bp = mem_sbrk(ALIGN(size))) == -1) {
+  if ((long)(bp = mem_sbrk((size))) == -1) {
     return NULL;
   }
 
   PUT(HDRP(bp), PACK(size, 0));
   PUT(FTRP(bp), PACK(size, 0));
   PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));
+  PUT_POINT(PREP(bp), NULL);
+  PUT_POINT(SUCP(bp), NULL);
   bp = coalesce(bp);
   insert_list(bp, choose_list(size));
+  CHECKHEAP(lineno)
   return bp;
 }
 
@@ -267,29 +316,48 @@ static void isinCorrectfreelist() {
 }
 
 static void *find_block(size_t size) {
-  char **list = choose_list(size);
-  char *bp = *list;
-  while (bp != NULL) {
-    if (GET_SIZE(HDRP(bp)) >= size) {
-      return bp;
-    } else {
-      bp = GET_POINT(SUCP(bp));
+  for (char **list = choose_list(size);
+       (void *)list != (void *)(heap_listp - WSIZE); list++) {
+    char *bp = *list;
+    while (bp != NULL) {
+      if (GET_SIZE(HDRP(bp)) >= size) {
+        return bp;
+      } else {
+        bp = GET_POINT(SUCP(bp));
+      }
     }
   }
-  return find_block(size * 2);
+  return NULL;
 }
 
 static void place(char *bp, size_t size) {
-  if (GET_SIZE(HDRP(bp)) >= (size_t)(3 * DSIZE)) {
-    int asize = GET_SIZE(HDRP(bp)) - size;
+  int asize = GET_SIZE(HDRP(bp)) - size;
+  if (asize >= (size_t)(2 * DSIZE)) {
     PUT(HDRP(bp), PACK(size, 1));
     PUT(FTRP(bp), PACK(size, 1));
     PUT(HDRP(NEXT_BLKP(bp)), PACK(asize, 0));
     PUT(FTRP(NEXT_BLKP(bp)), PACK(asize, 0));
-    insert_list(bp, choose_list(asize));
+    insert_list(NEXT_BLKP(bp), choose_list(asize));
   }
   PUT(HDRP(bp), PACK(GET_SIZE(HDRP(bp)), 1));
   PUT(FTRP(bp), PACK(GET_SIZE(HDRP(bp)), 1));
 }
 
-static void heap_cheker() { isinCorrectfreelist(); }
+static void inRange() {
+  for (char **list = list_32; (void *)list != (void *)(heap_listp - WSIZE);
+       list++) {
+    char *cur = *list;
+    while (cur) {
+      if (cur > heap_listp && (void *)cur <= mem_heap_hi()) {
+        cur = GET_POINT(SUCP(cur));
+      } else {
+        printf("block in free list is not in heap\n");
+      }
+    }
+  }
+}
+
+static void heap_checker() {
+  isinCorrectfreelist();
+  inRange();
+}
